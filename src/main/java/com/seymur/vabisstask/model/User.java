d407f1cd4671bdc45ea87a0842c24ca93bde1ba0
Seymur Mansurov <seymur.mansurov@a2z.az>
2021-06-22 17:50:56 +0400

    add swagger config and cors support

---
+++ src/main/java/com/seymur/vabisstask/model/User.java
@@ -0,0 +1,29 @@
+package com.seymur.vabisstask.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column(name = "is_enabled")
+    private int isEnabled;
+
+    @Column(name = "created", updatable = false)
+    private LocalDateTime createDate;
+
+
+}
