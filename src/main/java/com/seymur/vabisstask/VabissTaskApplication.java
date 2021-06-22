09447af3ec8106153b78262985e6db3bc9b0502d
Seymur Mansurov <seymur.mansurov@a2z.az>
2021-06-22 17:37:50 +0400

    start task

---
+++ src/main/java/com/seymur/vabisstask/VabissTaskApplication.java
@@ -0,0 +1,13 @@
+package com.seymur.vabisstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VabissTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VabissTaskApplication.class, args);
+    }
+
+}
